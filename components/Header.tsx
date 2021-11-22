308acec27fa9f618771d2b51f6a6c1659c16cc7b
elliothwang <elliothwang@gmail.com>
2021-11-22 01:58:11 -0800

    create & style navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,55 +1,95 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
 import ThemeButton from './ThemeButton';
 import styles from '../styles/Header.module.scss';
 
 const Header = () => {
+  const [dark, setDark] = useState(true);
+  const [sidebar, setSidebar] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleThemeChange = () => {
+    setDark(!dark);
+  };
+
+  const showSidebar = () => {
+    setSidebar(!sidebar);
+  };
+
+  useEffect(function mount() {
+    function onScroll() {
+      var scrollYDistance = window.scrollY;
+      scrollYDistance >= 200 ? setScrolled(true) : setScrolled(false);
+    }
+
+    window.addEventListener('scroll', onScroll);
+
+    return function unMount() {
+      window.removeEventListener('scroll', onScroll);
+    };
+  });
+
   return (
     <>
-    {/* mobile/tablet */}
-    {/* LEFT: dark/light mode button */}
-    {/* CENTER: logo  */}
-    {/* RIGHT: sidenav hamburger icon */}
-    {/* sidenav links */}
-
-    {/* pc */}
-    {/* LEFT: logo */}
-    {/* CENTER: links (About, Projects, Experience, Contact) */}
-    {/* RIGHT: dark/light mode button */}
-
-      <header className={styles.header}>
-        <ThemeButton />
-        
-        <nav>
-          <ul>
+      <div className={styles.sidebarBttn} onClick={showSidebar}>
+        <div className={sidebar ? styles.topBarActive : styles.topBar}></div>
+        <div className={sidebar ? styles.midBarActive : styles.midBar}></div>
+        <div className={sidebar ? styles.botBarActive : styles.botBar}></div>
+      </div>
+      <header
+        className={styles.header}
+        style={scrolled ? { transform: 'translateY(-100%)' } : null}
+      >
+        <nav className={styles.nav}>
+          <ul className={styles.list}>
             <li>
               <Link href="/">
-                <a className={styles.link}>LOGO</a>
+                <a className={styles.logo}>
+                  {/* <Image src="/public/images/imagetest.png" layout="fill" ></Image> */}
+                  L
+                </a>
               </Link>
             </li>
             <li>
               <Link href="/about">
-                <a className={styles.link}>About</a>
+                <a className={sidebar ? styles.activeLink : styles.link1}>
+                  About
+                </a>
               </Link>
             </li>
             <li>
               <Link href="/projects">
-                <a className={styles.link}>Projects</a>
+                <a className={sidebar ? styles.activeLink : styles.link2}>
+                  Projects
+                </a>
               </Link>
             </li>
             <li>
               <Link href="/experience">
-                <a className={styles.link}>Experience</a>
+                <a className={sidebar ? styles.activeLink : styles.link3}>
+                  Experience
+                </a>
               </Link>
             </li>
             <li>
               <Link href="/contact">
-                <a className={styles.link}>Contact</a>
+                <a className={sidebar ? styles.activeLink : styles.link4}>
+                  Contact
+                </a>
               </Link>
+            </li>
+            <li>
+              <div
+                className={sidebar ? styles.activeButton : styles.button}
+                onClick={handleThemeChange}
+              >
+                <ThemeButton />
+              </div>
             </li>
           </ul>
         </nav>
       </header>
-
     </>
   );
 };
--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -3,12 +3,12 @@ import styles from '../../styles/Projects.module.scss';
 
 const Projects = () => {
   return (
-    <>
+    <div className={styles.projects}>
       <h1>What have I made?</h1>
 
       {/* grid mapping with ProjectCards component */}
       <ProjectCard />
-    </>
+    </div>
   );
 };
 
