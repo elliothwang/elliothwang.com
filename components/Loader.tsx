503675a59aa88ec9c94506d84799777e54f5cea0
elliothwang <elliothwang@gmail.com>
2021-12-06 19:52:58 -0800

    install rebecca-purple

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styles from '../styles/Loader.module.scss';
 
+// TODO: create logo & loader animation to replace temporary ones in Loader & Navbar;
 const Loader = () => {
   return (
     <div className={styles.screen}>
