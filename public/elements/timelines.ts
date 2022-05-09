3b4756b6bc530ef5afde00a1a32b14d39dd2caf4
elliothwang <elliothwang@gmail.com>
2022-05-09 14:54:36 -0700

    add internship info

--- public/elements/timelines.ts
+++ public/elements/timelines.ts
@@ -10,6 +10,18 @@ type Props = {
 const timelines: Array<Props> = [
   {
     id: 1,
+    title: 'Software Engineering Internship',
+    location: 'Behaivior',
+    date: 'Jan 2022 - May 2022',
+    description: [
+      'Collaborated with a team of 4 to 6 developers and interns in the development of user-facing analytics dashboards.',
+      'Assisted in the migration from Redux to React Query and the implementation of Chakra UI.',
+      'Attended meetings with the development team and leadership to discuss product strategy and direction.',
+    ],
+    icon: 'work',
+  },
+  {
+    id: 2,
     title: 'Full-Stack SWE Bootcamp',
     location: 'General Assembly',
     date: 'Feb 2021 - May 2021',
@@ -19,19 +31,19 @@ const timelines: Array<Props> = [
     icon: 'school',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Laboratory Research Assistant',
     location: 'California State University, Northridge',
     date: 'Aug 2019 - Mar 2020',
     description: [
-      'Collaborated with a team of four to collect and analyze biomechanics research data.',
+      'Collaborated within a team of 4 to collect and analyze biomechanics research data.',
       'Ensured adequate documentation of protocol and maintained data in a well-organized repository.',
       'Tracked and prepared study-specific information utilizing databases, spreadsheets, and software tools.',
     ],
     icon: 'work',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Administrative Assistant',
     location: 'All Nations Church',
     date: 'Mar 2018 - Jan 2020',
@@ -44,18 +56,6 @@ const timelines: Array<Props> = [
   },
   {
     id: 4,
-    title: 'Emergency Dept. Volunteer',
-    location: 'Henry Mayo Newhall Hospital',
-    date: 'Mar 2019 - Mar 2020',
-    description: [
-      'Supported team of clinicians and nurses to ensure streamlined patient care.',
-      'Served as a liaison between patients and clinical team, assessing needs and relaying messages in a timely manner.',
-      'Assisted with on-boarding of new volunteers by leading protocol training.',
-    ],
-    icon: 'work',
-  },
-  {
-    id: 5,
     title: 'B.S. Kinesiology',
     location: 'California State University, Northridge',
     date: 'Mar 2018 - May 2020',
